501ec092cbc25a1bed97c600f5061f9b910f14ae
GerasHarutyunyan18 <harutyunyan18geras@gmail.com>
2025-12-24 21:56:18 +0400

    Init

---
+++ consent.cookiebot.com/c8188f85-4419-4ff0-b285-4097f032a4c9/cc.js
@@ -0,0 +1,9 @@
+if (console) {
+  var cookiedomainwarning =
+    "Error: The domain WWW.FRITESATELIER.COM is not authorized to show the cookie banner for domain group ID c8188f85-4419-4ff0-b285-4097f032a4c9. Please add it to the domain group in the Cookiebot Manager to authorize the domain.";
+  if (typeof console.warn === "function") {
+    console.warn(cookiedomainwarning);
+  } else {
+    console.log(cookiedomainwarning);
+  }
+}
